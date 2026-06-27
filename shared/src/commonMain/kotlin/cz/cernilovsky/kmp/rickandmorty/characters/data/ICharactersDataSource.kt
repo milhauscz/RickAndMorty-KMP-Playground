b7da423b772c29c2e1f6b8d921e3869d5270c1a7
Miloš Černilovský <cernilovsky@gmail.com>
2026-06-27 18:08:13 +0200

    feature: Localize UI strings and map network errors to user-facing messages
    - Add strings.xml with localized error messages, button labels, and UI copy
    - Add ErrorToStringResourceMapping to convert Throwable/DataError to StringResource
    - Replace all hardcoded strings in CharacterListScreen with stringResource() calls
    - Wire ErrorMessage and LoadingItemsError to show localized error text with retry

--- shared/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/data/ICharactersDataSource.kt
+++ shared/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/data/ICharactersDataSource.kt
@@ -2,6 +2,7 @@ package cz.cernilovsky.kmp.rickandmorty.characters.data
 
 import cz.cernilovsky.kmp.rickandmorty.characters.data.remote.CharactersResponseDto
 import cz.cernilovsky.kmp.rickandmorty.core.domain.DataError
+import cz.cernilovsky.kmp.rickandmorty.core.domain.Result
 
 interface ICharactersDataSource {
     suspend fun getCharacters(page: Int): Result<CharactersResponseDto, DataError.Remote>
---
+++ shared/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/core/ui/ErrorToStringResourceMapping.kt
@@ -0,0 +1,26 @@
+package cz.cernilovsky.kmp.rickandmorty.core.ui
+
+import cz.cernilovsky.kmp.rickandmorty.core.domain.DataError
+import cz.cernilovsky.kmp.rickandmorty.core.network.HttpClientException
+import org.jetbrains.compose.resources.StringResource
+import rickandmorty.shared.generated.resources.Res
+import rickandmorty.shared.generated.resources.error_no_internet
+import rickandmorty.shared.generated.resources.error_serialization
+import rickandmorty.shared.generated.resources.error_server
+import rickandmorty.shared.generated.resources.error_timeout
+import rickandmorty.shared.generated.resources.error_too_many_requests
+import rickandmorty.shared.generated.resources.error_unknown
+
+fun Throwable.toMessageRes(): StringResource = when (this) {
+    is HttpClientException -> error.toMessageRes()
+    else -> Res.string.error_unknown
+}
+
+private fun DataError.Remote.toMessageRes(): StringResource = when (this) {
+    DataError.Remote.NO_INTERNET      -> Res.string.error_no_internet
+    DataError.Remote.REQUEST_TIMEOUT  -> Res.string.error_timeout
+    DataError.Remote.TOO_MANY_REQUESTS-> Res.string.error_too_many_requests
+    DataError.Remote.SERVER           -> Res.string.error_server
+    DataError.Remote.SERIALIZATION    -> Res.string.error_serialization
+    DataError.Remote.UNKNOWN          -> Res.string.error_unknown
+}
