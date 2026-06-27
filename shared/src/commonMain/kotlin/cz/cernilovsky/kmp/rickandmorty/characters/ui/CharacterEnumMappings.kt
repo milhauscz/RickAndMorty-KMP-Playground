d1e76531df0ec3e475732687f4d7b91dd97fe066
Miloš Černilovský <cernilovsky@gmail.com>
2026-06-27 18:23:46 +0200

    feature: Add status dot indicator and trim UiCharacter to UI-relevant fields
    - Add CharacterEnumMappings with CharacterStatus/CharacterGender → StringResource and Color mappings
    - Add string resources for all CharacterStatus and CharacterGender values
    - Show colored dot (green/red/gray) next to status text in character row
    - Remove unused fields from UiCharacter (type, gender, origin, episode, url, created)
    - Delete leftover template files Greeting.kt and GreetingUtil.kt

---
+++ shared/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/ui/CharacterEnumMappings.kt
@@ -0,0 +1,26 @@
+package cz.cernilovsky.kmp.rickandmorty.characters.ui
+
+import cz.cernilovsky.kmp.rickandmorty.characters.domain.model.CharacterGender
+import cz.cernilovsky.kmp.rickandmorty.characters.domain.model.CharacterStatus
+import org.jetbrains.compose.resources.StringResource
+import rickandmorty.shared.generated.resources.Res
+import rickandmorty.shared.generated.resources.character_gender_female
+import rickandmorty.shared.generated.resources.character_gender_genderless
+import rickandmorty.shared.generated.resources.character_gender_male
+import rickandmorty.shared.generated.resources.character_gender_unknown
+import rickandmorty.shared.generated.resources.character_status_alive
+import rickandmorty.shared.generated.resources.character_status_dead
+import rickandmorty.shared.generated.resources.character_status_unknown
+
+fun CharacterStatus.toStringResource(): StringResource = when (this) {
+    CharacterStatus.Alive   -> Res.string.character_status_alive
+    CharacterStatus.Dead    -> Res.string.character_status_dead
+    CharacterStatus.Unknown -> Res.string.character_status_unknown
+}
+
+fun CharacterGender.toStringResource(): StringResource = when (this) {
+    CharacterGender.Female     -> Res.string.character_gender_female
+    CharacterGender.Male       -> Res.string.character_gender_male
+    CharacterGender.Genderless -> Res.string.character_gender_genderless
+    CharacterGender.Unknown    -> Res.string.character_gender_unknown
+}
--- shared/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/ui/CharacterMapper.kt
+++ shared/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/ui/CharacterMapper.kt
@@ -2,18 +2,11 @@ package cz.cernilovsky.kmp.rickandmorty.characters.ui
 
 import cz.cernilovsky.kmp.rickandmorty.characters.domain.model.Character
 
-fun Character.toUiCharacter(favorite: Boolean = false) = UiCharacter(
+fun Character.toUiCharacter() = UiCharacter(
     id = id,
     name = name,
     status = status,
     species = species,
-    type = type,
-    gender = gender,
-    origin = origin,
     location = location,
     image = image,
-    episode = episode,
-    url = url,
-    created = created,
-    favorite = favorite,
 )
