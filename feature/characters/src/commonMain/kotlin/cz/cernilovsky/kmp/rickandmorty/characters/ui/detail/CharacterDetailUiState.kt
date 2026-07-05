77135ffeb147fceb7eac789f1bcc01e01278ebf5
Miloš Černilovský <cernilovsky@gmail.com>
2026-07-05 21:18:36 +0200

    refactor: Annotate CharacterDetailUiState @Immutable
    Its detail/isLoading fields are already stable; StringResource is a cross-module
    type the compiler can't infer, so annotate the all-val state class as immutable to
    mark it stable.

--- feature/characters/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/ui/detail/CharacterDetailUiState.kt
+++ feature/characters/src/commonMain/kotlin/cz/cernilovsky/kmp/rickandmorty/characters/ui/detail/CharacterDetailUiState.kt
@@ -1,7 +1,9 @@
 package cz.cernilovsky.kmp.rickandmorty.characters.ui.detail
 
+import androidx.compose.runtime.Immutable
 import org.jetbrains.compose.resources.StringResource
 
+@Immutable
 data class CharacterDetailUiState(
     val detail: UiCharacterDetail? = null,
     val isLoading: Boolean = true,
